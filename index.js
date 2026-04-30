acb75669f962b6afd83d1f398f7d717d6845c34c
kalmyon <kalkalmyon@gmail.com>
2026-04-30 15:58:03 +0900

    make pingpong

---
+++ index.js
@@ -0,0 +1,26 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`ログイン成功: ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
